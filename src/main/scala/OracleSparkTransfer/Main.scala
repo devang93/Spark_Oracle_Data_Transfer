e8142e500e90aa73cc069473e6ebd5e3feda212c
Devang Anilkumar Patel <depatel@depatel012657.starbucks.net>
2018-03-26 19:52:37 -0700

    sample main application added.

--- src/main/scala/OracleSparkTransfer/Main.scala
+++ src/main/scala/OracleSparkTransfer/Main.scala
@@ -1,12 +1,27 @@
 package OracleSparkTransfer
 
+import java.util.Properties
+
+import org.apache.spark.sql.SparkSession
+
 /**
-  * Created by devang3 on 3/24/2018.
+  * Created by devang93 on 3/24/2018.
   */
 object Main {
 
   def main(args: Array[String]): Unit = {
 
-    
+    val spark = SparkSession.builder().master("local[*]").appName("Spark_Oracle_Data_Transfer").getOrCreate()
+
+    val jdbcURL = s"jdbc:oracle:thin:@//${args(3)}/${args(2)}"
+    val oracleProperties = new Properties()
+    oracleProperties.setProperty("user", args(0))
+    oracleProperties.setProperty("password", args(1))
+    oracleProperties.setProperty("driver", "oracle.jdbc.driver.OracleDriver")
+
+    val oracleDF = spark.read.jdbc(url = jdbcURL, table = s"(${args(4)}) oracle_data_pull", properties = oracleProperties)
+
+    oracleDF.show()
+
   }
 }
